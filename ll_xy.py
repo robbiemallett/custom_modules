43bb3f2a3b89025f61d160f4a4e9b21c64e4679a
Robbie Mallett <robbiemallett@gmail.com>
2022-02-04 12:57:27 +0000

    adding llxy

---
+++ ll_xy.py
@@ -0,0 +1,47 @@
+from pyproj import Proj, Transformer
+import numpy as np
+
+def lonlat_to_xy(coords_1, coords_2, hemisphere, inverse=False):
+
+ 
+
+    """Converts between longitude/latitude and EASE xy coordinates.
+ 
+    Args:
+        lon (float): WGS84 longitude
+        lat (float): WGS84 latitude
+        hemisphere (string): 'n' or 's'
+        inverse (bool): if true, converts xy to lon/lat
+ 
+    Returns:
+        tuple: pair of xy or lon/lat values
+    """
+
+ 
+
+    EASE_Proj = {'n': 'EPSG:3408',
+                 's': 'EPGS:3409'}
+    
+    WGS_Proj = 'EPSG:4326'
+    
+    for coords in [coords_1, coords_2]: assert isinstance(coords,(np.ndarray,list))
+
+    if inverse == False: # lonlat to xy
+        
+        lon, lat = coords_1, coords_2
+        
+        transformer = Transformer.from_crs(WGS_Proj, EASE_Proj[hemisphere])
+        
+        x, y = transformer.transform(lat, lon)
+        
+        return (x, y)
+
+    else: # xy to lonlat
+        
+        x, y = coords_1, coords_2
+        
+        transformer = Transformer.from_crs(EASE_Proj[hemisphere], WGS_Proj)
+        
+        lat, lon = transformer.transform(x, y)
+        
+        return (lon, lat)
