8d6adc2e032bc22c2ed753215f589899b04ceda3
Robbie Mallett <robbiemallett@gmail.com>
2022-07-15 17:25:44 +0100

    faster_regridder

--- regrid.py
+++ regrid.py
@@ -1,6 +1,9 @@
 from scipy.interpolate import griddata
 import pyproj as proj
 import numpy as np
+from scipy.spatial import Delaunay
+from scipy.interpolate import LinearNDInterpolator
+import tqdm
 
 args = proj.Proj(proj="aeqd", lat_0=90, lon_0=0, datum="WGS84", units="m")
 
@@ -21,5 +24,30 @@ def regrid(data_in,
                     np.array(data_in).ravel(),
                     (xout,yout),
                     method=method)
+    
+    return(output)
+
+
+def regrid_fast(data_in,
+           lon_in,
+           lat_in,
+           lon_out,
+           lat_out,
+           fill_val=np.nan):
+
+    xout, yout = proj.transform(crs_wgs, args, np.array(lon_out),np.array(lat_out))
+
+    xin, yin = proj.transform(crs_wgs, args, np.array(lon_in),np.array(lat_in))
+
+    points = np.column_stack((xin.ravel(),yin.ravel()))
+    tri = Delaunay(points)  # Compute the triangulation
+
+    output = np.full((data_in.shape[0],xout.shape[0],xout.shape[1]),fill_val)    
+    
+    for i in tqdm.trange(0,data_in.shape[0]):
+
+        interpolator = LinearNDInterpolator(tri, data_in[i].ravel())
+
+        output[i] = interpolator((xout,yout))
     
     return(output)
